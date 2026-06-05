1e16ae172eb01ee29c5a514b3120bfb4547df1db
Sohaib Nawaz <sohaib.nawaz@mmcgbl.com>
2026-06-05 15:14:02 +0500

    little change

--- lib/core/store/store_preference.dart
+++ lib/core/store/store_preference.dart
@@ -15,11 +15,19 @@ class StorePreference extends Store {
 
   @override
   Either<StoreFailure, T> read<T>(String key) {
-    if (_prefs.containsKey(key)) {
-      String? jsonString = _prefs.getString(key);
-      if (jsonString != null) {
-        T value = jsonDecode(jsonString) as T;
-        return right(value);
+    try {
+      if (_prefs.containsKey(key)) {
+        String? jsonString = _prefs.getString(key);
+        if (jsonString != null) {
+          T value = jsonDecode(jsonString) as T;
+          return right(value);
+        } else {
+          return left(
+            StoreFailure(
+              error: kDebugMode ? "No value found for key: $key" : "No Data",
+            ),
+          );
+        }
       } else {
         return left(
           StoreFailure(
@@ -27,19 +35,15 @@ class StorePreference extends Store {
           ),
         );
       }
-    } else {
-      return left(
-        StoreFailure(
-          error: kDebugMode ? "No value found for key: $key" : "No Data",
-        ),
-      );
+    } catch (e) {
+      return left(StoreFailure(error: e.toString()));
     }
   }
 
   @override
   Either<StoreFailure, T> write<T>(String key, T value) {
     try {
-      _prefs.setString(key, value.toString());
+      _prefs.setString(key, jsonEncode(value));
       return right(value);
     } catch (e) {
       return left(StoreFailure(error: e.toString()));
